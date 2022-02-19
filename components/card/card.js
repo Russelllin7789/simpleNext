cd082047a1cc6e19796f95be59c65136881689f9
RussellLin <superbr7789@gmail.com>
2022-02-20 02:43:15 +0800

    Add the left CSS

--- components/card/card.js
+++ components/card/card.js
@@ -43,25 +43,64 @@ const Card = (props) => {
         <div className={classes.class_title}>{name}</div>
         <div className={classes.class_desc}>{desc}</div>
         <div className={classes.rate_container}>
-          {/* <img src={}></img> */}
+          <svg viewBox="0 0 576 512">
+            <path
+              fill="currentColor"
+              d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"
+            />
+          </svg>
+          <svg viewBox="0 0 576 512">
+            <path
+              fill="currentColor"
+              d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"
+            />
+          </svg>
+          <svg viewBox="0 0 576 512">
+            <path
+              fill="currentColor"
+              d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"
+            />
+          </svg>
+          <svg viewBox="0 0 576 512">
+            <path
+              fill="currentColor"
+              d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"
+            />
+          </svg>
+          <svg viewBox="0 0 536 512">
+            <path
+              fill="currentColor"
+              d="M508.55 171.51L362.18 150.2 296.77 17.81C290.89 5.98 279.42 0 267.95 0c-11.4 0-22.79 5.9-28.69 17.81l-65.43 132.38-146.38 21.29c-26.25 3.8-36.77 36.09-17.74 54.59l105.89 103-25.06 145.48C86.98 495.33 103.57 512 122.15 512c4.93 0 10-1.17 14.87-3.75l130.95-68.68 130.94 68.7c4.86 2.55 9.92 3.71 14.83 3.71 18.6 0 35.22-16.61 31.66-37.4l-25.03-145.49 105.91-102.98c19.04-18.5 8.52-50.8-17.73-54.6zm-121.74 123.2l-18.12 17.62 4.28 24.88 19.52 113.45-102.13-53.59-22.38-11.74.03-317.19 51.03 103.29 11.18 22.63 25.01 3.64 114.23 16.63-82.65 80.38z"
+            />
+          </svg>
           <span>{average_rating}</span>
           <span>({rating_count})</span>
         </div>
         <div className={classes.tag_container}>
           {tags.map((tag, index) => (
-            <div key={index}>#{tag}</div>
+            <div className={classes.tag_itself} key={index}>
+              #{tag}
+            </div>
           ))}
         </div>
         <div className={classes.price_container}>
-          <div>{currency}$</div>
-          <div>{sale_price}</div>
-          <div>
+          <div className={classes.special_sale}>{currency}$</div>
+          <div className={classes.special_sale}>{sale_price}</div>
+          <div className={classes.original_price}>
             List price {currency}${list_price}
           </div>
         </div>
         <div className={classes.action_container}>
           <button>Free trial</button>
-          <button>Details</button>
+          <div className={classes.detail_container}>
+            Details
+            <svg viewBox="0 0 448 512">
+              <path
+                fill="currentColor"
+                d="M190.5 66.9l22.2-22.2c9.4-9.4 24.6-9.4 33.9 0L441 239c9.4 9.4 9.4 24.6 0 33.9L246.6 467.3c-9.4 9.4-24.6 9.4-33.9 0l-22.2-22.2c-9.5-9.5-9.3-25 .4-34.3L311.4 296H24c-13.3 0-24-10.7-24-24v-32c0-13.3 10.7-24 24-24h287.4L190.9 101.2c-9.8-9.3-10-24.8-.4-34.3z"
+              />
+            </svg>
+          </div>
         </div>
       </div>
     </div>
