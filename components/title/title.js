8c3197e1f6265e4d78e0619ccc2aec3a5433361c
RussellLin <superbr7789@gmail.com>
2022-02-19 12:34:44 +0800

    Add Title style and page design

--- components/title/title.js
+++ components/title/title.js
@@ -2,8 +2,10 @@ import classes from "./title.module.css";
 
 const Title = () => {
   return (
-    <div className={classes.title_line}>
+    <div className={classes.all_container}>
+      <hr className={classes.title_line}></hr>
       <h2 className={classes.title}>強檔多益教材</h2>
+      <hr className={classes.title_line}></hr>
     </div>
   );
 };
--- pages/demo/index.js
+++ pages/demo/index.js
@@ -1,10 +1,11 @@
 // domain.com/demo
 import Card from "../../components/card/Card";
 import Title from "../../components/title/title";
+import classes from "./demo.module.css";
 
 const HomePage = () => {
   return (
-    <section>
+    <section className={classes.all_container}>
       <Title />
       <Card />
     </section>
